fee50e97842cc6235183f2a117e965c2260b3df5
timi000 <63570149+timi000@users.noreply.github.com>
2020-09-29 22:32:35 -0400

    updated app

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from models import create_classes
 import os
 from flask_sqlalchemy import SQLAlchemy
@@ -109,44 +108,3 @@ def corona():
 
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import mission_to_mars
-
-# Create an instance of Flask
-app = Flask(__name__)
-
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/planet_app")
-
-
-# Route to render index.html template using data from Mongo
-@app.route("/")
-def home():
-
-    # Find one record of data from the mongo database
-    m_data = mongo.db.mars.find_one()
-    print(m_data)
-
-    # Return template and data
-    return render_template("index.html", mars=m_data)
-
-
-# Route that will trigger the scrape function
-@app.route("/scrape")
-def scrape():
-
-    # Run the scrape function
-    mars_data = mission_to_mars.scrape_info()
-
-    # Update the Mongo database using update and upsert=True
-    mongo.db.mars.update_one({}, {"$set": mars_data}, upsert=True)
-
-    # Redirect back to home page
-    return redirect("/")
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
->>>>>>> ca4b36b0d971b3715dac9dabccce28b2a3b1bba5
